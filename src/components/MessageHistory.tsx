9f761cba93cdff1ce7fb7ffa0149ea84678ac2c9
JulieSoboleva <ju.soboll@gmail.com>
2024-07-31 13:47:57 +0300

    Delete console logs

--- src/components/MessageHistory.tsx
+++ src/components/MessageHistory.tsx
@@ -19,16 +19,12 @@ export default function MessageHistory({list}: IMessages) {
                 const msg: IMsg = {
                     id: item.id, time:item.time, text:item.text
                 }
-                console.log(item.type)
                 if (item.type === 'message') {
-                    console.log('message')
                     return (<Message key={msg.id} msg={msg} from={item.from}/>);
                 }
                 if (item.type === 'response') {
-                    console.log('response')
                     return (<Response key={msg.id} msg={msg} from={item.from}/>);
                 }
-                console.log('typing')
                 return (<Typing key={msg.id} msg={msg} from={item.from}/>);
             })}
         </ul>
